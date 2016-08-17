7e242c7f9d5fdf24086c242079c194fd7f48fdcc
iamjohnxiong <iamjohnxiong@gmail.com>
2016-08-16 22:32:56 -0400

    Quote Alarm Initial
    Quote alarm project

---
+++ QuoteAlarm/QuoteAlarm/QuoteAlarmTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  QuoteAlarm.swift
+//  QuoteAlarm
+//
+//  Created by John Xiong on 8/16/16.
+//  Copyright © 2016 John Xiong. All rights reserved.
+//
+
+import UIKit;
+
+
+class QuoteAlarmTableViewCell : UITableViewCell {
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var timeframeLabel: UILabel!
+}
